752854eb482f40ac95099bdb2a91599b55722893
megaIp <mhmdmsao188@gmail.com>
2022-08-23 02:45:49 +0300

    Add New Folders

---
+++ mega-ip/src/Main/Header/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css"
+function Header() {
+  return (
+    <div className="header">
+      <div className = "logoHeader">logo</div>
+
+      <React.Fragment>
+        <ul className="headerList">
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default Header;
